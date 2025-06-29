29de7e175ebcf2c58f4d9cc1e132e90199d18f17
saoirsenacilla <saoirsenacilla@gmail.com>
2025-06-29 09:35:36 -0400

    1.21.1-Forge Initial Commit (Broken)

--- src/main/java/net/saoirse/saoirseqol/item/ModToolTiers.java
+++ src/main/java/net/saoirse/saoirseqol/item/ModToolTiers.java
@@ -5,7 +5,6 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
-import net.minecraftforge.common.TierSortingRegistry;
 import net.saoirse.saoirseqol.SaoirseQOL;
 import net.saoirse.saoirseqol.util.ModTags;
 
